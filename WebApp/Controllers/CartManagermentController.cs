f863ec0b67e837ecdb43ea4b5c9f36f46d57763c
QuocTien <sinlee349@gmail.com>
2024-06-26 20:28:38 +0700

    update QuanLyRoHang

---
+++ WebApp/Controllers/CartManagermentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToolsApp.Authentication;
+
+namespace ToolsApp.Controllers
+{
+    public class CartManagermentController : BaseController
+    {
+        // GET: CartManagerment
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Insert()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult hinhAnh_View()
+        {
+            return PartialView();
+        }
+        public ActionResult ThongTinBaiViet_View()
+        {
+            return PartialView();
+        }
+        public ActionResult ThongTinKhachHang_View()
+        {
+            return PartialView();
+        }
+
+    }
+}
